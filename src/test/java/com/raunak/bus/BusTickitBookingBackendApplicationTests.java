6c100c81135316c16792582877d5126cfab02de2
raunakagrawal <araunak.coolboy@gmail.com>
2023-11-23 11:52:24 +0530

    Initial

---
+++ src/test/java/com/raunak/bus/BusTickitBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raunak.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTickitBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
